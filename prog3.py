ba2a6b95ce8fb4c760563bcc18ad96c697c357cd
Varsha Verma <42858281+varshaverma231@users.noreply.github.com>
2018-10-15 12:31:27 -0700

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,12 @@
+abcd=set(list(map(chr, range(97, 123))))
+abcd2=set(list(map(chr, range(65, 91))))
+aa=abcd.union(abcd2)
+print(aa)
+a=input()
+if a in aa:
+    if a in ['a','e','o','i','u','A','E','O','I','U']:
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("Invalid")
